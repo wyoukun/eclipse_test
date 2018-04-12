5acd3528d602b27eba26d0feeaa6ca39cb1d1b4a
jack <timor0000@163.com>
2018-04-12 18:45:12 +0800

    the sec time commit

---
+++ maven_test/src/main/java/com/proud/test_001/Test_sec.java
@@ -0,0 +1,7 @@
+package com.proud.test_001;
+
+public class Test_sec {
+	public static void main(String[] args) {
+		System.out.println("test second time!!!");
+	}
+}
---
+++ maven_test/src/main/java/com/proud/test_001/Test_third.java
@@ -0,0 +1,7 @@
+package com.proud.test_001;
+
+public class Test_third {
+public static void main(String[] args) {
+	System.out.println("test the third time!!");
+}
+}
