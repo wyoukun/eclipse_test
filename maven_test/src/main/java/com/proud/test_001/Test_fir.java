750521a470faa68d4367d60f6e3505d2f5590b09
jack <timor0000@163.com>
2018-04-12 18:27:32 +0800

    java the first

---
+++ maven_test/src/main/java/com/proud/test_001/Test_fir.java
@@ -0,0 +1,13 @@
+package com.proud.test_001;
+
+public class Test_fir {
+	public static void main(String[] args) {
+		for(int i=0;i<7;i++){
+			for(int j=0;j<i-1;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println("the first time change!!");
+	}
+}
