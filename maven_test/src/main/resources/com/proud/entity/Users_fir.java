3f9aee3b03657db5efe08db77816f190ea77cb33
jack <timor0000@163.com>
2018-04-13 16:46:35 +0800

    commit data

--- maven_test/src/main/resources/com/proud/entity/Users_fir.java
+++ maven_test/src/main/resources/com/proud/entity/Users_fir.java
@@ -1,5 +1,8 @@
 package com.proud.entity;
 
 public class Users_fir {
-
+	
+	private void mian() {
+		System.out.println("test first!!!");
+	}
 }
--- maven_test/src/test/resources/com/proud/en/Test_admin.java
+++ maven_test/src/test/resources/com/proud/en/Test_admin.java
@@ -1,5 +1,8 @@
 package com.proud.en;
 
 public class Test_admin {
-
+	
+	public static void main(String[] args) {
+		System.out.println("test admin first fa");
+	}
 }
